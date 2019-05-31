bbf25aeee958808cbe8f728a01db9d81252f3f22
Firealem Erko <firealemg@yahoo.com>
2019-05-31 12:44:17 -0400

    added get endpoint for projects

--- api/projectRouter.js
+++ api/projectRouter.js
@@ -2,4 +2,21 @@ const express = require("express");
 const db = require("../data/helpers/projectModel");
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.get()
+      .then(projects => {
+        res.status(200).json({
+          success: true,
+          projects
+        })
+      })
+      .catch(err => {
+        res.status(500).json({
+          success: false,
+          error: "The projects does not exist or could not be retrived"
+        })
+      })
+  })
+
+
 module.exports = router;
